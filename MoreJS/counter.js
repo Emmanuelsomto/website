eeb8d9fabdf72a5b93c5381895d10ff8953dd81e
Emmanuel <geleagbaiemmanuel@gmail.com>
2026-05-22 18:40:52 +0100

    More new projects

---
+++ MoreJS/counter.js
@@ -0,0 +1,20 @@
+function printCharacters(str) {
+  for (const char of str) {
+    console.log(char)
+  }
+};
+
+printCharacters("hello");
+
+function getMatchedWordCount(sentence, match) {
+  let count = 0;
+  return count
+};
+
+console.log(getMatchedWordCount(["I", "really","really", "really", "like", "to", "code"], "really"));
+
+console.log(`Checking "${word}" against "${match}" | Running count: ${count}`);
+
+function getMatchedWordCount(sentence, match) {
+  let count = 1;
+};
